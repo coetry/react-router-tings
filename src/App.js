5be2f18b98b0dc0cc9907d78d34af38d0933fc3f
Allen Hai <allenhai03@gmail.com>
2018-09-27 14:01:10 -0500

    🤷🏽‍♂️

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route 
+} from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <>
+          <Nav />
+          <main>
+            <Route exact path="/" component={CircleRoute} />
+            <Route path="/:id" component={CircleRoute} />
+          </main>
+        </>
+      </Router>
     );
   }
+}
+
+function Nav () {
+  return <div className="side-nav">
+    <nav>
+      <Link to="/">index</Link>
+      <Link to="/home">home</Link>
+      <Link to="/space">space</Link>
+      <Link to="/houston">h-town</Link>
+    </nav>
+  </div>
+}
+
+function CircleRoute ({ match }) {
+  return <>
+    <svg>
+      <circle cx="50" cy="50" r="50" fill={randomColor()} />
+    </svg>
+    { match.params.id 
+      ? <code>{ match.params.id }</code>
+      : <code>index</code>
+    }
+  </>
+
+}
+
+function randomColor () {
+  return `rgb(${Math.floor(Math.random() * 256)},
+              ${Math.floor(Math.random() * 256)},
+              ${Math.floor(Math.random() * 256)})`
 }
 
 export default App;
